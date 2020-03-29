1e397685a1488e9ba087b8d7966dc47264ac3c9c
jjunoo <49094710+jjunoo@users.noreply.github.com>
2020-03-29 19:08:22 -0300

    Update Rio Grande

--- functions/index2.js
+++ functions/index2.js
@@ -7,39 +7,39 @@ let url = "smtps://<EMAIL>%40gmail.com:"+encodeURIComponent('<SENHA>') + "@smtp.
 let transporter = nodemailer.createTransport(url);
 
 exports.enviarEmail = functions.https.onRequest((req, res) => {
-  cors(req, res, () => {
-    let remetente = '"Portal Ajuda+" <portalajudamais@gmail.com>';
+ cors(req, res, () => {
+   let remetente = '"Portal Ajuda+" <portalajudamais@gmail.com>';
 
-    let name = req.body['name'];
-    let email = req.body['email']; // lista de e-mails destinatarios separados por ,
-    let assunto = req.body['subject'];
-    let mensagem = req.body['message'];
+   let name = req.body['name'];
+   let email = req.body['email']; // lista de e-mails destinatarios separados por ,
+   let assunto = req.body['subject'];
+   let mensagem = req.body['message'];
 
-    let email1 = {
-        from: remetente,
-        to: remetente, 
-        subject: assunto,
-        text: "corpo",
-        html: mensagem
-    };
-    let email2 = {
-        from: remetente,
-        to: email, 
-        subject: "Equipe Portal Ajuda+",
-        text: "corpo",
-        html: "Olá, muito obrigado por entrar em contato com o Portal Ajuda+, em breve entraremos em contato com você!" 
-    };
-    transporter.sendMail(email1, (error, info) => {
-        if (error) {
-          return console.log(error);
-        }
-        console.log('Mensagem %s enviada: %s', info.messageId, info.response);
-    });
-    transporter.sendMail(email2, (error, info) => {
-        if (error) {
-          return console.log(error);
-        }
-        console.log('Mensagem %s enviada: %s', info.messageId, info.response);
-    });
-  });
+   let email1 = {
+       from: remetente,
+       to: remetente,
+       subject: assunto,
+       text: "corpo",
+       html: mensagem
+   };
+   let email2 = {
+       from: remetente,
+       to: email,
+       subject: "Equipe Portal Ajuda+",
+       text: "corpo",
+       html: "Olá, muito obrigado por entrar em contato com o Portal Ajuda+, em breve entraremos em contato com você!"
+   };
+   transporter.sendMail(email1, (error, info) => {
+       if (error) {
+         return console.log(error);
+       }
+       console.log('Mensagem %s enviada: %s', info.messageId, info.response);
+   });
+   transporter.sendMail(email2, (error, info) => {
+       if (error) {
+         return console.log(error);
+       }
+       console.log('Mensagem %s enviada: %s', info.messageId, info.response);
+   });
+ });
 });
